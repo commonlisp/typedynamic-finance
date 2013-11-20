92d030b02f905188dcd23adb93de7d0003fc3c44
George Kuan <commonlisp@gmail.com>
2013-11-19 20:01:26 -0800

    Event controller work

--- app/controllers/Event.scala
+++ app/controllers/Event.scala
@@ -3,15 +3,23 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import com.mongodb.casbah.Imports._
+
 object Event extends Controller {
-	
-def newEvent() = Action { implicit req =>
-  Ok("newEvent")
 
-}
+  val coll = MongoConnection()("typedynamic")("events")	
 
-def show(id : String) = Action {
-  Ok("show " + id)
-}
+  def newEvent() = Action { implicit req =>
+    Ok("newEvent")
+  }
+
+  def show(id : String) = Action {
+    val o = MongoDBObject("id" -> id)
+    coll.findOne(o) match {
+      case None => Ok("Not found")
+      case Some(m) => Ok("Found " + m)
+    }
+    Ok("Error")
+  }
 
 }
